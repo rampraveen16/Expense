9c4339af8ce237458e01ac105a34e88770baa883
Rampraveen Indrajith <ram.praveen16@gmail.com>
2020-05-08 22:45:07 -0400

    Updates

---
+++ precache-manifest.21ac5ba7a352f2cfb7795f2ae01e8e12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9caa7252340acb1d02262fe33fe42240",
+    "url": "/Expense/index.html"
+  },
+  {
+    "revision": "19c0818aa2faa07a46fd",
+    "url": "/Expense/static/css/main.775eae5b.chunk.css"
+  },
+  {
+    "revision": "8a80933cd11f9ee73ad2",
+    "url": "/Expense/static/js/2.cdd1a915.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Expense/static/js/2.cdd1a915.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19c0818aa2faa07a46fd",
+    "url": "/Expense/static/js/main.0f98c9da.chunk.js"
+  },
+  {
+    "revision": "f0f08d64b6b55c611a79",
+    "url": "/Expense/static/js/runtime-main.6c8dbb35.js"
+  },
+  {
+    "revision": "46a3fbea9cc477e64e5303c912d411ea",
+    "url": "/Expense/static/media/delete.46a3fbea.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7095f4877800bfebb9987326ead2af8b.js"
+  "/Expense/precache-manifest.21ac5ba7a352f2cfb7795f2ae01e8e12.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Expense/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
